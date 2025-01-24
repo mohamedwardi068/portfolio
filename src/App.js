c852df32dc817196d8813c82702f7e2fefe16442
mohamedwardi068 <mohamedwardi068@gmail.com>
2025-01-24 17:24:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Nav from "./nav/Nav.js"
+
+const App=()=> {
+  return (
+    <div className="App">
+      App
+    </div>
+  );
+}
+
+export default App;
