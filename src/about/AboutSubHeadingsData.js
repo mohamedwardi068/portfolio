5f8faaa656c0895597a93a2ad47dae45da94c577
mohamedwardi068 <mohamedwardi068@gmail.com>
2025-01-31 22:05:13 +0100

    31jan

---
+++ src/about/AboutSubHeadingsData.js
@@ -0,0 +1,84 @@
+const AboutSubHeadingData = {
+  1: [
+    {
+      title: 'About Me',
+      content:
+        [<>
+          <p>
+            I’m Mohamed, a full-stack web developer and <br />
+            student passionate about building functional<br />
+            web applications. I enjoy learning and creating <br />
+            innovative solutions while balancing my studies<br />
+            and coding projects.
+          </p>
+
+        </>]
+
+    }
+  ],
+  2: [
+    {
+      title: '2024 baccalauréat degree',
+      content:
+        [<>
+          <p>
+            I’m Mohamed, a full-stack web developer and <br />
+            student passionate about building functional<br />
+            web applications. I enjoy learning and creating <br />
+            innovative solutions while balancing my studies<br />
+            and coding projects.
+          </p>
+
+        </>]
+
+    },
+    {
+      title: '2024 Bachelors degree',
+      content:
+        [<>
+          <p>
+            I’m Mohamed, a full-stack web developer and <br />
+            student passionate about building functional<br />
+            web applications. I enjoy learning and creating <br />
+            innovative solutions while balancing my studies<br />
+            and coding projects.
+          </p>
+
+        </>]
+
+    }
+  ],
+  3: [
+    {
+      title: '2024 learning',
+      content:
+        [<>
+          <p>
+            I’m Mohamed, a full-stack web developer and <br />
+            student passionate about building functional<br />
+            web applications. I enjoy learning and creating <br />
+            innovative solutions while balancing my studies<br />
+            and coding projects.
+          </p>
+
+        </>]
+
+    },
+    {
+      title: '2025 freelancer',
+      content:
+        [<>
+          <p>
+            I’m Mohamed, a full-stack web developer and <br />
+            student passionate about building functional<br />
+            web applications. I enjoy learning and creating <br />
+            innovative solutions while balancing my studies<br />
+            and coding projects.
+          </p>
+
+        </>]
+
+    }
+  ]
+};
+export default AboutSubHeadingData
