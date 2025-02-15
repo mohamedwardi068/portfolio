bfa09ed03561b1d0eea6bfe3c6237709dc156a51
mohamedwardi068 <mohamedwardi068@gmail.com>
2025-02-15 17:38:19 +0100

    15feb

---
+++ src/skills/SkillsData.js
@@ -0,0 +1,63 @@
+const skills={
+    1:[
+       { title:"HTML",
+        level:6,
+
+    },
+    { title:"CSS",
+        level:5,
+        
+    },
+    { title:"JavaScript",
+        level:5,
+        
+    },
+    { title:"TypeScript",
+        level:4,
+        
+    },
+    { title:"Tailwind CSS",
+        level:6,
+        
+    },
+    { title:"React JS",
+        level:3,
+        
+    },
+    { title:"Figma",
+        level:2,
+        
+    },
+    
+
+    ],
+    2:[
+        { title:"Node JS",
+         level:3,
+ 
+     },
+     { title:"Mongo DB",
+         level:3,
+         
+     },
+     { title:"GIT",
+         level:3,
+         
+     },
+     { title:"PHP",
+         level:5,
+         
+     },
+     { title:"SQL ",
+         level:6,
+         
+     },
+     { title:"Express JS",
+         level:3,
+         
+     },
+ 
+ 
+     ]
+}
+export default skills;
