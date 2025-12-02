50d4406a4bf5b533e74fe76b7f9925baab14f678
mohamedwardi068 <mohamedwardi068@gmail.com>
2025-12-02 21:50:13 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./nav/Nav.js"
 import About from "./about/About.js"
 import Skills from "./skills/Skills.js";
 import Projects from "./projects/Projects.js"
 import Contact from "./contact/Contact.js"
 import "./styles/app.css"
+import "./styles/cv-button.css"
 import Background from "./background/Background.js";
 import PlayerStats from "./playerStats/playerStats.js"
 
-const App=()=> {
+const App = () => {
   return (
     <>
-    <Router>
-    <Nav/>
-    <Background/>
-    <Routes>
-      <Route path="/" element={<About/>}/>
-      <Route path="/skills" element={<Skills/>}/>
-      <Route path="/projects" element={<Projects/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      
-      
-      </Routes>
-      <PlayerStats />
+      <Router>
+        <Nav />
+        <Background />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+
+
+        </Routes>
+        <PlayerStats />
+        <a href="/cv.pdf" download className="download-cv-btn">
+          DOWNLOAD CV
+        </a>
       </Router></>
   );
 }
